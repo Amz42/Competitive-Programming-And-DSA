b040418912ed5bf9ee6eb0f892c2983bc71b1912
Aman Gupta <aman.gupta.rkg6@gmail.com>
2021-02-24 22:20:59 +0530

    added more solutions

---
+++ Competitive Programming/Codeforces/Codeforces Round #681 (Div. 2, based on VK Cup 2019-2020 - Final)/C. The Delivery Dilemma.cpp
@@ -0,0 +1,72 @@
+// Question: https://codeforces.com/contest/1443/problem/C
+// Solution: https://codeforces.com/contest/1443/submission/97489457
+
+/*                                              _                    _  _  ____
+                                               / \   _ __ ___  ____ | || ||___ \
+                                              / _ \ | '_ ` _ \|_  / | || |_ __) |
+                                             / ___ \| | | | | |/ /  |__   _/ __/
+                                            /_/   \_\_| |_| |_/___|    |_||_____|
+*/
+#include <bits/stdc++.h>
+#define ll          long long
+#define u_map       unordered_map
+#define sht         short
+#define pb          push_back
+#define mkpr        make_pair
+#define NL          cout<<"\n";
+#define optimize    ios::sync_with_stdio(0);cin.tie(0);
+#define fori(a,n)   for(ll int i=a;i<(ll int)n;i++)
+#define forj(a,n)   for(ll int j=a;j<(ll int)n;j++)
+#define MOD         1e9+7
+#define ten5        1e5
+#define all(v)      v.begin(),v.end()
+#define sort_d(v)   sort(all(v),greater<ll int>());
+#define maxv(v)     *max_element(all(v))
+#define LOWCASE(s)  transform(all(s),s.begin(),::tolower);
+#define UPCASE(s)   transform(all(s),s.begin(),::toupper);
+#define setbits(b)  __builtin_popcount(b);
+#define tests       ll int t;cin>>t;while(t--)
+using namespace std;
+struct hash_pair{
+    template<class T1,class T2> 
+    size_t operator()(const pair<T1,T2>& p)const{
+        auto h1=hash<T1>{}(p.first);
+        auto h2=hash<T2>{}(p.second); 
+        return h1^h2;
+    }
+};
+int main(){
+    optimize
+    /////// Write from here
+    
+    tests{
+        ll int n, x, y; cin>>n;
+        vector<pair<ll int,ll int>> v;
+        
+        ll int AA[n], BB[n];
+        
+        fori(0,n) cin>>AA[i];
+        fori(0,n) cin>>BB[i];
+        
+        ll int tim=0;
+        fori(0,n){
+            x = AA[i];
+            y = BB[i];
+            v.push_back({x,y});
+        }
+        
+        sort(v.begin(), v.end());
+        
+        tim = v[n-1].first;
+        ll int l = 0;
+        for(ll int i=n-2;i>=0;i--){
+            l += v[i+1].second;
+            tim = min(tim, max(v[i].first, l) );
+        }
+        
+        tim  = min(tim, accumulate(BB, BB+n, 0LL));
+        cout<<tim; NL
+    }
+    
+	return 0;
+}
